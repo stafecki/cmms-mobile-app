813d01e59c17b55cebdf064de0dc6f8ef80dcd9d
stafec <stafecki303@gmail.com>
2026-04-22 10:44:43 +0200

    feat: project structure and constants

---
+++ app/app/src/main/java/com/example/cmms/utils/Constants.java
@@ -0,0 +1,14 @@
+package com.example.cmms.utils;
+
+public class Constants {
+    public static final String BASE_URL = "http://10.0.2.2:3000/";
+
+    public static final String DB_NAME = "cmms_db";
+
+    public static final String PREFS_NAME = "cmms_prefs";
+    public static final String KEY_TOKEN = "access_token";
+    public static final String KEY_USER_ID = "user_id";
+    public static final String KEY_USER_ROLE = "user_role";
+
+    private Constants() {}
+}
